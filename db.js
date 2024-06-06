07b3f26b9fb48058b1740ae710ab5cda3fdfdc5a
Sjaiod <delwar.saj2008@gmail.com>
2024-06-06 16:42:14 +0600

    s

--- db.js
+++ db.js
@@ -3,6 +3,11 @@ const mongoose=require("mongoose")
 mongoose.set(`strictPopulate`,false)
 mongoose.set(`strictQuery`,false)
 
-mongoose.connect(process.env.DB_URl).then(()=>{
+mongoose.connect(process.env.DB_URl, {
+
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true"
+  
+  }).then(()=>{
     console.log(`Connected`);
 }).catch(err=>{console.log(err);})
